d4afc2e341f05b749a8657badc5296974f014f6e
Okafor Nelson <okafornel@gmail.com>
2022-04-06 13:01:19 +0100

    Create method for second question

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -29,15 +30,33 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"Please select an option for question 1",Toast.LENGTH_SHORT).show();
         } else if (radioButton.isChecked()) {
             totalCorrectAnswer += 1;
-        } else totalCorrectAnswer = totalCorrectAnswer;
+        }
     }
 
     /**
+     * Question 2 Method
+     */
+    public void questionTwo() {
+        CheckBox messi = findViewById(R.id.messi);
+        CheckBox neymar = findViewById(R.id.neymar);
+        CheckBox suarez = findViewById(R.id.suarez);
+        CheckBox xavi = findViewById(R.id.xavi);
+
+        if (!messi.isChecked() && !neymar.isChecked() && !suarez.isChecked() && !xavi.isChecked()) {
+            Toast.makeText(this,"Please select an option for question 2",Toast.LENGTH_SHORT).show();
+        } else if (messi.isChecked() && suarez.isChecked() && neymar.isChecked()) {
+            totalCorrectAnswer += 1;
+        }
+    }
+
+
+    /**
      * Submit Button Method
-     * @param view
+     * @param view tag
      */
     public void submitButton(View view) {
         questionOne();
+        questionTwo();
         Log.i("Main Activity", "submitButton: " + totalCorrectAnswer);
         Toast.makeText(this,"You scored " + totalCorrectAnswer + " out of 5",Toast.LENGTH_SHORT).show();
     }
